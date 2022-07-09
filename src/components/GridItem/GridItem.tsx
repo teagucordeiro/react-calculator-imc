e5dba292c521d95a5a522d50c100d31034afb8cd
ThiagoOliveiraCordeiro <thiagoty52@gmail.com>
2022-07-09 09:35:50 -0300

    feat: calculator is working

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -20,6 +20,13 @@ export function GridItem({ item }: Props) {
         />
       </div>
       <div className={styles.gridTitle}>{item.title}</div>
+
+      {item.yourImc && 
+        <div className={styles.yourImc}>
+          Seu IMC é de {item.yourImc} kg/m<sup>2</sup>
+        </div>
+      }
+
       <div className={styles.gridInfo}>
         <>
           IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
